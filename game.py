c472b06c26c0ad27a2d0ec7acc360c82b6b63c5b
Eva Redensek <eva.redensek@gmail.com>
2014-11-28 18:18:31 +0100

    dodala lepo zeleno - roza kaco

--- game.py
+++ game.py
@@ -12,12 +12,14 @@ import random
 
 from bolivianAnaconda import BolivianAnaconda
 from congoWaterCobra import CongoWaterCobra
+from boomslang import Boomslang
 
 # Nato v spisek SNAKES dodate razred, ki predstavlja vaso kaco
 
 SNAKES = [
     BolivianAnaconda,
     CongoWaterCobra,
+    Boomslang
 ]
 
 # Od tu naprej se ni treba nicesar dotikati
